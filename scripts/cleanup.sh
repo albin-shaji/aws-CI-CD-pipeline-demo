35a5e15247df7bd0263dfbba8332fc48c6bf5361
Albin Shaji <67319498+albin-shaji@users.noreply.github.com>
2026-05-27 01:12:45 +0530

    Update ownership of application directory
    Change ownership of APP_DIR from 'ubuntu' to 'jenkins'.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 set -e
 
 APP_DIR="/home/ubuntu/app"
 
 mkdir -p $APP_DIR
-
 rm -rf ${APP_DIR:?}/*
-
-chown -R ubuntu:ubuntu $APP_DIR
-
+chown -R jenkins:jenkins $APP_DIR
 chmod -R 755 $APP_DIR
